b99b391b9590a7445c14f49cd09fbc1dd83667e6
themitnick <yaotyrone@gmail.com>
2024-06-19 01:09:28 +0200

    Reactive Form

--- src/app/components/forms/reactive/reactive.component.ts
+++ src/app/components/forms/reactive/reactive.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive.component.html',
   styleUrl: './reactive.component.css'
 })
 export class ReactiveComponent {
 
+  studentForm: FormGroup = new FormGroup({
+    firstName: new FormControl("",
+      [Validators.required, Validators.minLength(3)]
+    ),
+    lastName: new FormControl(),
+    userName: new FormControl("", 
+      [Validators.email]
+    ),
+    city: new FormControl(),
+    state: new FormControl(),
+    zipCode: new FormControl("",
+      [Validators.required, Validators.maxLength(5)]
+    ),
+    isAcceptTerms: new FormControl()
+  });
+
+  formValue: any;
+
+  onSave() {
+    debugger
+    this.formValue = this.studentForm.value;
+  }
 }
